86abe78039962f65db79ca62ce011997c1c3c578
TTAJihoon <jh91082@tta.or.kr>
2025-04-18 13:40:06 +0900

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import login_test_view
 from . import views
 
 urlpatterns = [
     path('welcome/', views.welcome, name='welcome'),
+    path('test-login/', login_test_view),
 ]
