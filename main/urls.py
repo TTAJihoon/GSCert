ddc2b6aa30fdd64eddd04101136541117a31ce41
TTAJihoon <jh91082@tta.or.kr>
2025-04-18 13:43:43 +0900

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from .views import login_test_view
 from . import views
 
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='main/registration/login.html'), name='login'),
     path('welcome/', views.welcome, name='welcome'),
     path('test-login/', login_test_view),
 ]
