cd51e5913236d924f7664361535f0983d1dcb2b9
TTAJihoon <jh91082@tta.or.kr>
2025-04-18 13:39:21 +0900

    Update views.py

--- main/views.py
+++ main/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 @login_required
 def welcome(request):
     return render(request, 'main/welcome.html')
+
+def login_test_view(request):
+    return render(request, 'main/registration/login.html')
