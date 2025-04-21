870377cf332039b91d438bc52da58a503759e3ed
TTAJihoon <jh91082@tta.or.kr>
2025-04-21 14:51:59 +0900

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=y(y=6f&6-*s)*0yvp=wx5ss_94ukpe044&gesp3^&u!(%wkn#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
