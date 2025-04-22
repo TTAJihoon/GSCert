4960d6bd1969fa882a41c14e822668c25eee5723
TTAJihoon <jh91082@tta.or.kr>
2025-04-22 15:58:40 +0900

    Update login.py

--- main/views/login.py
+++ main/views/login.py
@@ -1,1 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
+@login_required
+def welcome(request):
+    return render(request, 'main/welcome.html')
+
+def login_test_view(request):
+    return render(request, 'main/registration/login.html')
+
+def index(request):
+    return render(request, 'main/index.html')
