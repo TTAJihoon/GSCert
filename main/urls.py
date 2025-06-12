c8d379131fd28502d2dec8d786816f2104ba3289
TTAJihoon <jh91082@tta.or.kr>
2025-06-12 16:57:35 +0900

    ollama 내부 호출 적용

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from main.views.login import login_test_view
 from main.views.login import welcome
 from main.views.login import index
-from main.views.LLaMa import rag_query
+from main.views.chatbot import chat_gpt
 from . import views
 
 urlpatterns = [
@@ -11,5 +11,5 @@ urlpatterns = [
     path('welcome/', welcome),
     path('index/', index),
     path('test-login/', login_test_view),
-    path('test/', rag_query, name='llama_test'),
+    path('chatbot/', chat_gpt, name='llama_test'),
 ]
