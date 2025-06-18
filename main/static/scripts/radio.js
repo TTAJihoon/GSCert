8f9eb161779d842763ddec4267a8408209800e94
TTAJihoon <jh91082@tta.or.kr>
2025-06-18 13:42:31 +0900

    Update radio.js

--- main/static/scripts/radio.js
+++ main/static/scripts/radio.js
@@ -3,15 +3,21 @@ function toggleInputs() {
   const companyInput = document.getElementById('company');
   const productInput = document.getElementById('product');
   const commentInput = document.getElementById('comment');
+  const startDateInput = document.getElementById('startDate');
+  const endDateInput = document.getElementById('endDate');
 
   if (historyRadio.checked) {
     commentInput.disabled = true;
     companyInput.disabled = false;
     productInput.disabled = false;
+    startDateInput.disabled = false;
+    endDateInput.disabled = false;
   } else {
     commentInput.disabled = false;
     companyInput.disabled = true;
     productInput.disabled = true;
+    startDateInput.disabled = false;
+    endDateInput.disabled = false;
   }
 }
 
