695b7f9892dd0a420472a1facfb0f196ab1852cd
TTAJihoon <jh91082@tta.or.kr>
2025-07-02 15:19:14 +0900

    Update sendValue.py

--- main/views/testing/sendValue.py
+++ main/views/testing/sendValue.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 from django.shortcuts import render
 from .history import GS_history
-from .chatbot import run_openai_GPT
+from .similar_GPT import run_openai_GPT
 
 def search_history(request):
     if request.method == 'POST':
