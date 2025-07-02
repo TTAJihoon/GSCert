b23ff332fde23b56af485513678abac2da731813
TTAJihoon <jh91082@tta.or.kr>
2025-07-02 16:17:32 +0900

    Update similar_GPT.py

--- main/views/testing/similar_GPT.py
+++ main/views/testing/similar_GPT.py
@@ -1,6 +1,6 @@
 import os
 from openai import OpenAI
-from main.utils import reload_reference
+from main.utils.reload_reference import getREF
 
 # OpenAI API 키로 클라이언트 초기화
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
