ac64f93efd3985a29e8566c298c882b9cdf5da9e
TTAJihoon <jh91082@tta.or.kr>
2025-07-07 09:17:56 +0900

    Update generate_answer.py

--- main/utils/generate_answer.py
+++ main/utils/generate_answer.py
@@ -1,5 +1,5 @@
-from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
 from openai import OpenAI
 import os
 
