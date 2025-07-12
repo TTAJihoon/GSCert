2b855872072f3f59de7562ab47779231a455ab2e
TTAJihoon <jh91082@tta.or.kr>
2025-07-12 23:32:30 +0900

    Update set_date.js

--- main/static/scripts/set_date.js
+++ main/static/scripts/set_date.js
@@ -1,14 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const endDateInput = document.getElementById('end_date');
-  
-  if (!endDateInput.value) {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = ('0' + (today.getMonth() + 1)).slice(-2);
-    const day = ('0' + today.getDate()).slice(-2);
-
-    endDateInput.value = `${year}-${month}-${day}`;
-  }
+  document.getElementById('five-years-btn').click();
 });
 
 function setYearsAgo(years) {
