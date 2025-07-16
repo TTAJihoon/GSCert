3568127c643909267be91888f5afb01bd2797b3e
TTAJihoon <jh91082@tta.or.kr>
2025-07-16 09:13:19 +0900

    Create loading_mark.js

---
+++ main/static/scripts/loading_mark.js
@@ -0,0 +1,4 @@
+document.getElementById('queryForm').addEventListener('submit', function() {
+  document.getElementById('loadingIndicator').style.display = 'block';
+  document.querySelector('.main-content').style.display = 'none';
+});
