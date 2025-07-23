f42477f7cb40a6b628c02aed7e8eaf42c7e021b7
TTAJihoon <jh91082@tta.or.kr>
2025-07-23 21:00:39 +0900

    Update loading_mark.js

--- main/static/scripts/loading_mark.js
+++ main/static/scripts/loading_mark.js
@@ -1,18 +1,26 @@
-document.getElementById('queryForm').addEventListener('submit', function() {
-  document.getElementById('loadingIndicator').style.display = 'block';
-  document.querySelector('.main-content').style.display = 'none';
+// Loading_mark
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('queryForm');
+
+    form.addEventListener('submit', function() {
+      document.getElementById('loadingIndicator').style.display = 'block';
+      document.querySelector('.main-content').style.display = 'none';
+    });
 });
 
-document.getElementById('queryForm').addEventListener('submit', function(e) {
-    // 입력 필드 값을 가져옵니다.
-    const gsnum = document.getElementById('gsnum').value.trim();
-    const project = document.getElementById('project').value.trim();
-    const company = document.getElementById('company').value.trim();
-    const product = document.getElementById('product').value.trim();
+// Null_check
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('queryForm');
 
-    // 네 가지 필드가 모두 비어 있으면 알림창을 띄웁니다.
-    if (!gsnum && !project && !company && !product) {
-        e.preventDefault();  // 폼 제출 방지
-        alert('검색 조건을 입력해주세요');
-    }
+    form.addEventListener('submit', function(e) {
+        const gsnum = document.getElementById('gsnum').value.trim();
+        const project = document.getElementById('project').value.trim();
+        const company = document.getElementById('company').value.trim();
+        const product = document.getElementById('product').value.trim();
+
+        if (!gsnum && !project && !company && !product) {
+            e.preventDefault();
+            alert('검색 조건을 입력해주세요');
+        }
+    });
 });
