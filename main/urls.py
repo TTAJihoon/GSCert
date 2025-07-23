a238d785d87725eceb14fc823e58bea04f571ab7
TTAJihoon <jh91082@tta.or.kr>
2025-07-23 20:12:12 +0900

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,17 +1,16 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
-from main.views.init import index, search, security, genspark, test
-from main.views.testing.sendValue import search_history
+from main.views.init import index, security, genspark, test
+from main.views.testing.history import history
 from main.utils.reload_reference import reload_reference_view
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('index/', index, name='index'),
-    path('search/', search, name='search'),
+    path('history/', history, name='history'),
+    #path('similar/', similar, name='similar'),
     path('security/', security, name='security'),
     path('genspark/', genspark, name='genspark'),
     path('test/', test, name='genspark2'),
-    path('search-history/', search_history, name='search-h'),
     path('reload-reference/', reload_reference_view, name='reload_reference'),
-    
 ]
