575be60d278bad637e76f436509b07ef9bd3ddbd
TTAJihoon <jh91082@tta.or.kr>
2025-08-01 20:26:15 +0900

    Update apps.py

--- main/apps.py
+++ main/apps.py
@@ -6,5 +6,4 @@ class MainConfig(AppConfig):
     name = 'main'
 
     def ready(self):
-        from main.utils import reload_reference  # 캐시 로직 있는 모듈 import
-        reload_reference.reload_reference_dataframe()  # 서버 시작 시 1회만 실행
+        return;
