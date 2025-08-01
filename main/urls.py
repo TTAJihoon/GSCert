036c94688654e730af8e226fb24e65db9b193e2d
TTAJihoon <jh91082@tta.or.kr>
2025-08-01 20:28:27 +0900

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from main.views.init import index, similar, security, genspark, test
 from main.views.testing.history import history
 from main.views.testing.similar_summary import summarize_document
-from main.utils.reload_reference import reload_reference_view
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
@@ -14,5 +13,4 @@ urlpatterns = [
     path('security/', security, name='security'),
     path('genspark/', genspark, name='genspark'),
     path('test/', test, name='genspark2'),
-    path('reload-reference/', reload_reference_view, name='reload_reference'),
 ]
