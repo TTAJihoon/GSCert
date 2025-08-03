d041fd8347a25e04e4a5860d30d05e511618dc05
TTAJihoon <jh91082@tta.or.kr>
2025-08-04 00:36:00 +0900

    Update similar_compare.py

--- main/views/testing/similar_compare.py
+++ main/views/testing/similar_compare.py
@@ -36,7 +36,7 @@ def compare_from_index(text):
 
   D, I = index.search(query_vec, k=30)
 
-  matched_db_ids = db_ids[I[0]]
+  matched_db_ids = [int(db_ids[i]) for i in I[0]]
   tables = select_data_from_db(matched_db_ids)[::-1]
   print(tables)
   print("가장 유사한 DB id:", matched_db_ids)
