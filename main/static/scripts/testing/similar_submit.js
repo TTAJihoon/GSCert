7c7a24fe018ff5818833ee4b701995529cdb8a9e
TTAJihoon <jh91082@tta.or.kr>
2025-08-09 22:44:01 +0900

    Update similar_submit.js

--- main/static/scripts/testing/similar_submit.js
+++ main/static/scripts/testing/similar_submit.js
@@ -79,7 +79,7 @@ document.addEventListener('DOMContentLoaded', function () {
               </tbody>
             </table>
           </div>
-          <div class="similarity-score">유사도 95%</div>
+          <--! <div class="similarity-score">유사도 95%</div> -->
         </div>
         <div class="product-description">
           ${(row['제품설명'] || '-').replace(/\n/g, '<br>')}
