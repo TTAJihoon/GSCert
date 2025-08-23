5f510f8800e204cf0147332da1cc16a8b7dc6ebe
TTAJihoon <jh91082@tta.or.kr>
2025-08-23 23:58:03 +0900

    Update n-gram.py

--- main/management/commands/n-gram.py
+++ main/management/commands/n-gram.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from main.utils.embedding_to_faiss import build_faiss_from_db
+from main.utils.n-gram_to_faiss import build_ngram_table
 
 class Command(BaseCommand):
     help = "DB 데이터를 FAISS로 임베딩합니다."
