1575d80dd55b513fdfd6e77ce24448d51061e6ee
TTAJihoon <jh91082@tta.or.kr>
2025-08-27 09:42:24 +0900

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,16 +1,20 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
-from main.views.init import index, similar, security, genspark, test
+from main.views.init import index, similar, security, prdinfo, genspark, test
 from main.views.testing.history import history
 from main.views.testing.similar_summary import summarize_document
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('index/', index, name='index'),
+    
     path('history/', history, name='history'),
     path('similar/', similar, name='similar'),
     path('summarize_document/', summarize_document, name='summarize_document'),
     path('security/', security, name='security'),
+
+    path('prdinfo/', prdinfo, name='prdinfo'),
+    
     path('genspark/', genspark, name='genspark'),
     path('test/', test, name='genspark2'),
 ]
