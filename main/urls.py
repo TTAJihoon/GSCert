2bcb1d6d2e377a55ab3937f88de3cdc38f73173d
TTAJihoon <jh91082@tta.or.kr>
2025-08-30 20:37:23 +0900

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,7 @@ from main.views.testing.similar_summary import summarize_document
 
 from main.views.certy.prdinfo_generate import generate_prdinfo
 from main.views.certy.prdinfo_URL import source_excel_view
+from main.views.certy.prdinfo_download import download_filled_prdinfo
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
@@ -20,6 +21,7 @@ urlpatterns = [
     path('prdinfo/', prdinfo, name='prdinfo'),
     path('generate_prdinfo/', generate_prdinfo, name='generate_prdinfo'),
     path('source-excel/', source_excel_view, name='source-excel'),
+    path("download-filled/", download_filled_prdinfo, name="download_filled"),
     
     path('genspark/', genspark, name='genspark'),
     path('test/', test, name='genspark2'),
