396ad347aefe83dded834695b15ef05fc960c5de
TTAJihoon <jh91082@tta.or.kr>
2025-09-08 17:48:48 +0900

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    status = models.CharField(max_length=20, default="PENDING")  # PENDING/RUNNING/DONE/ERROR
+    final_link = models.TextField(blank=True, null=True)         # 복사된 문장 저장(임시로 여기 활용)
+    error = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
