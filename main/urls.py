d65d5de8bbbefcecd102c2539bd8e39b6f594bab
TTAJihoon <jh91082@tta.or.kr>
2025-09-08 17:47:50 +0900

    ECM 업데이트

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 from main.views.init import index, similar, security, prdinfo, genspark, test
 
 from main.views.testing.history import history
 from main.views.testing.similar_summary import summarize_document
+
+from main.views.testing.history_ECMbtn import start_job, job_status
 
 from main.views.certy.prdinfo_generate import generate_prdinfo
 from main.views.certy.prdinfo_URL import source_excel_view
@@ -17,6 +19,12 @@ urlpatterns = [
     path('similar/', similar, name='similar'),
     path('summarize_document/', summarize_document, name='summarize_document'),
     path('security/', security, name='security'),
+    
+    # RQ 대시보드 (작업 큐 모니터링)
+    path("django-rq/", include("django_rq.urls")),
+    # API: 작업 생성 / 상태 조회
+    path("api/run-job/", start_job, name="start_job"),
+    path("api/job/<uuid:job_id>/", job_status, name="job_status"),
 
     path('prdinfo/', prdinfo, name='prdinfo'),
     path('generate_prdinfo/', generate_prdinfo, name='generate_prdinfo'),
