240b8ae8716fbee0c6a10a4013fb5194addc31d3
TTAJihoon <jh91082@tta.or.kr>
2025-10-14 23:14:57 +0900

    Update asgi.py

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,25 +1,13 @@
-"""
-ASGI config for myproject project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import main.routing  # 앱명에 맞게 수정
+import playwright_job.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            main.routing.websocket_urlpatterns  # 앱명에 맞게 수정
-        )
-    ),
+    "websocket": URLRouter(
+            playwright_job.routing.websocket_urlpatterns
+        ),
 })
