695b5c3fddf9fe7a07bdba3d0b56a399624c01bf
TTAJihoon <jh91082@tta.or.kr>
2025-10-15 22:00:40 +0900

    Update routing.py

--- playwright_job/routing.py
+++ playwright_job/routing.py
@@ -1,11 +1,6 @@
-from django.urls import re_path
+from django.urls import path
 from . import consumers
-import logging; logger = logging.getLogger(__name__)
-
-logger.warning(">>> playwright_job.websocket_urlpatterns 로딩됨")
 
 websocket_urlpatterns = [
-    # ws://서버주소/ws/run_job/ 경로로 웹소켓 연결이 오면
-    # PlaywrightJobConsumer가 처리하도록 설정합니다.
-    re_path(r'ws/run_job/$', consumers.PlaywrightJobConsumer.as_asgi()),
+    path("ws/run_job/", consumers.PlaywrightJobConsumer.as_asgi()),
 ]
