a8adf9dae7ff55137ee7f696db89d6a56e3fddea
TTAJihoon <jh91082@tta.or.kr>
2025-10-16 14:57:18 +0900

    Update routing.py

--- myproject/routing.py
+++ myproject/routing.py
@@ -1,9 +1,6 @@
-from channels.routing import URLRouter
-from django.urls import path
 import main.routing
 import playwright_job.routing
 
-# 'websocket_urlpatterns' 이름으로 합쳐서 export
 websocket_urlpatterns = (
     main.routing.websocket_urlpatterns
     + playwright_job.routing.websocket_urlpatterns
