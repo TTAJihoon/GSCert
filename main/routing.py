8f9096b16e5dac904a29d5ad6123951e6e8fb9c4
TTAJihoon <jh91082@tta.or.kr>
2025-10-16 14:58:01 +0900

    Update routing.py

--- main/routing.py
+++ main/routing.py
@@ -1,8 +1,7 @@
-from django.urls import path, re_path
+from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    # task_id가 영숫자+하이픈(예: UUID)도 허용되게
+    # task_id에 하이픈(예: UUID)도 허용하려면 \w -> [\w-]
     re_path(r"^ws/status/(?P<task_id>[\w-]+)/$", consumers.StatusConsumer.as_asgi()),
-    # 또는 path 변환자 직접 정의해서 path()로 쓰는 방법도 있음
 ]
