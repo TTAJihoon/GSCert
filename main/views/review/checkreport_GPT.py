f2279e0102091e08689e85020c2e9cc409dfb2c4
TTAJihoon <jh91082@tta.or.kr>
2025-10-27 00:31:14 +0900

    Update checkreport_GPT.py

--- main/views/review/checkreport_GPT.py
+++ main/views/review/checkreport_GPT.py
@@ -3,6 +3,7 @@ import json
 from django.http import JsonResponse, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
+from textwrap import dedent
 from openai import OpenAI
 
 # ---------- 내부 호출용 핵심 로직 ----------
