7c0ce27aba08006003a958f852171e7ecc7f6454
TTAJihoon <jh91082@tta.or.kr>
2026-01-12 20:03:23 +0900

    Update similar_submit.js

--- main/static/scripts/testing/similar_submit.js
+++ main/static/scripts/testing/similar_submit.js
@@ -100,7 +100,17 @@ console.log('response:', data.response);
                   </tbody>
                 </table>
               </div>
-              <div class="similarity-score">유사도 ${simPercent}%</div>
+              <div>
+                <table>
+                    <tr>
+                        <td>
+                            <div class="similarity-score" id="similarity-score">유사도</div>
+                        </td>
+                        <td>
+                            <button class="download-btn"><i class="fas fa-download"></i></button>
+                        </td>
+                    </tr>
+                </table>
             </div>
             <div class="product-description">
               ${(row['제품설명'] || '-').replace(/\n/g, '<br>')}
