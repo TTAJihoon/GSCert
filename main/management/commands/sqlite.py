0248a92ad43853e5b26607bc7b3ea897d6911e0d
TTAJihoon <jh91082@tta.or.kr>
2026-01-12 15:29:46 +0900

    Add files via upload

--- main/management/commands/sqlite.py
+++ main/management/commands/sqlite.py
@@ -1,18 +1,18 @@
 from django.core.management.base import BaseCommand
-from main.utils.csv_to_sqlite import convert_csv_to_sqlite
+from main.utils.xlsx_to_sqlite import convert_xlsx_to_sqlite
 
 class Command(BaseCommand):
-    help = "CSV파일을 SQLite DB로 변환하여 저장합니다."
+    help = "XLSX 파일을 SQLite DB로 변환하여 저장합니다."
 
     def add_arguments(self, parser):
-        parser.add_argument("csv_path", type=str, help="입력 CSV 파일 경로")
+        parser.add_argument("xlsx_path", type=str, help="입력 XLSX 파일 경로")
         parser.add_argument("db_path", type=str, help="출력 SQLite DB 파일 경로")
 
     def handle(self, *args, **options):
-        csv_path = options["csv_path"]
+        xlsx_path = options["xlsx_path"]
         db_path = options["db_path"]
 
-        self.stdout.write(f"▶ CSV 파일: {csv_path}")
+        self.stdout.write(f"▶ XLSX 파일: {xlsx_path}")
         self.stdout.write(f"▶ SQLite DB: {db_path}")
-
-        convert_csv_to_sqlite(csv_path, db_path)
+        
+        convert_xlsx_to_sqlite(xlsx_path, db_path)
